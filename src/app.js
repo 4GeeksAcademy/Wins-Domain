55cf8428ec7662da924169517495b1b40a464253
WinstonLesuT <138252771+WinstonLesuT@users.noreply.github.com>
2023-10-13 23:50:34 +0000

    domain generator

--- src/app.js
+++ src/app.js
@@ -2,10 +2,32 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function generateDomainName() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  const domainArray = [];
+  for (let pronoun1 of pronoun) {
+    for (let adj1 of adj) {
+      for (let noun1 of noun) {
+        domainArray.push(`${pronoun1} ${adj1} ${noun1}`);
+        console.log(`${pronoun1} ${adj1} ${noun1}`);
+      }
+    }
+  }
+  return domainArray;
+}
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector("#btn").addEventListener("click", () => {
+    const names = generateDomainName();
+    const listNames = document.getElementById("domainList");
+
+    names.forEach(word => {
+      const el = document.createElement("li");
+      el.textContent = word;
+      listNames.appendChild(el);
+    });
+  });
+
 };
